13fd84ab6c380164889884cedc838941f3db9ec0
Juraj Jarmek <badjjj@live.com>
2021-08-09 16:04:10 +0100

    test

--- 5. learningNode/05_project_folder/server.js
+++ 5. learningNode/05_project_folder/server.js
@@ -2,6 +2,27 @@ let express = require('express');
 let app = express();
 
 app.use(express.static(__dirname));
+
+let messages = [
+  {
+    name: 'JJ',
+    message: 'Hi',
+  },
+  {
+    name: 'Jane',
+    message: 'Hello',
+  },
+];
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
 let server = app.listen(3000, () => {
   console.log('server listening on port', server.address().port);
 });
