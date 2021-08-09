de16b0497b224688a2088f18495b87dac2b3325e
Juraj Jarmek <badjjj@live.com>
2021-08-09 16:25:52 +0100

    change

--- 5. learningNode/05_project_folder/server.js
+++ 5. learningNode/05_project_folder/server.js
@@ -1,17 +1,14 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 let messages = [
-  {
-    name: 'JJ',
-    message: 'Hi',
-  },
-  {
-    name: 'Jane',
-    message: 'Hello',
-  },
+  { name: 'Tim', message: 'Hi' },
+  { name: 'Jane', message: 'Hello' },
 ];
 
 app.get('/messages', (req, res) => {
@@ -19,10 +16,10 @@ app.get('/messages', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
-  console.log(req.body);
+  messages.push(req.body);
   res.sendStatus(200);
 });
 
 let server = app.listen(3000, () => {
-  console.log('server listening on port', server.address().port);
+  console.log('server is listening on port', server.address().port);
 });
