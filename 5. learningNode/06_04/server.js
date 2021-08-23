21c925b5ec7bfa934b4c6397b5985e4993f194ec
jjcoke <87483334+jjcoke@users.noreply.github.com>
2021-08-23 17:04:24 +0100

    test

--- 5. learningNode/06_04/server.js
+++ 5. learningNode/06_04/server.js
@@ -1,48 +1,46 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var dbUrl = 'mongodb://user:user@ds155424.mlab.com:55424/learning-node'
+var dbUrl = 'mongodb://user:user@ds155424.mlab.com:55424/learning-node';
 
 var Message = mongoose.model('Message', {
-    name: String,
-    message: String
-})
+  name: String,
+  message: String,
+});
 
 app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) =>{
-        res.send(messages)
-    })
-})
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
 
 app.post('/messages', (req, res) => {
-    var message = new Message(req.body)
+  var message = new Message(req.body);
 
-    message.save((err) => {
-        if (err)
-            sendStatus(500)
+  message.save((err) => {
+    if (err) sendStatus(500);
 
-        io.emit('message', req.body)
-        res.sendStatus(200)
-    })
-
-})
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  });
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
-})
+  console.log('a user connected');
+});
 
 mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
-    console.log('mongo db connection', err)
-})
+  console.log('mongo db connection', err);
+});
 
 var server = http.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
-})
+  console.log('server is listening on port', server.address().port);
+});
