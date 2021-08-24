ba12b2b924c1f8ad355be4d3a0e26d94f599676d
jjcoke <87483334+jjcoke@users.noreply.github.com>
2021-08-24 17:04:17 +0100

    test

--- 5. learningNode/08_03/server.js
+++ 5. learningNode/08_03/server.js
@@ -1,64 +1,59 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-mongoose.Promise = Promise
+mongoose.Promise = Promise;
 
-var dbUrl = 'mongodb://user:user@ds155424.mlab.com:55424/learning-node'
+var dbUrl = 'mongodb://user:user@ds155424.mlab.com:55424/learning-node';
 
 var Message = mongoose.model('Message', {
-    name: String,
-    message: String
-})
+  name: String,
+  message: String,
+});
 
 app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) => {
-        res.send(messages)
-    })
-})
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
 
 app.post('/messages', async (req, res) => {
+  try {
+    var message = new Message(req.body);
 
-    try {
-        var message = new Message(req.body)
+    var savedMessage = await message.save();
 
-        var savedMessage = await message.save()
+    console.log('saved');
 
-        console.log('saved')
+    var censored = await Message.findOne({ message: 'badword' });
 
-        var censored = await Message.findOne({ message: 'badword' })
+    if (censored) await Message.remove({ _id: censored.id });
+    else io.emit('message', req.body);
 
-        if (censored)
-            await Message.remove({ _id: censored.id })
-        else
-            io.emit('message', req.body)
-
-        res.sendStatus(200)
-    } catch (error) {
-        res.sendStatus(500)
-        return console.error(error)
-    } finally {
-        console.log('message post called')
-    }
-})
-
-
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    return console.error(error);
+  } finally {
+    console.log('message post called');
+  }
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
-})
+  console.log('a user connected');
+});
 
 mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
-    console.log('mongo db connection', err)
-})
+  console.log('mongo db connection', err);
+});
 
 var server = http.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
-})
+  console.log('server is listening on port', server.address().port);
+});
