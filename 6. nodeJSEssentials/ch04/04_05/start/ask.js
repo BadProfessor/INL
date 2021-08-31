ad69d7c1e8052701ee856fb2312819f64cd38823
jjcoke <87483334+jjcoke@users.noreply.github.com>
2021-08-31 16:24:28 +0100

    test

--- 6. nodeJSEssentials/ch04/04_05/start/ask.js
+++ 6. nodeJSEssentials/ch04/04_05/start/ask.js
@@ -1,10 +1,10 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question("How do you like Node? ", answer => {
+rl.question('How do you like Node? ', (answer) => {
   console.log(`Your answer: ${answer}`);
 });
