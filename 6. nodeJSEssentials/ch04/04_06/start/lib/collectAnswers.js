19c0dab7b6da0a2bf0bbf606a0d0e3c5ff3bebcc
jjcoke <87483334+jjcoke@users.noreply.github.com>
2021-09-07 11:21:32 +0100

    test

--- 6. nodeJSEssentials/ch04/04_06/start/lib/collectAnswers.js
+++ 6. nodeJSEssentials/ch04/04_06/start/lib/collectAnswers.js
@@ -1,14 +1,14 @@
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-module.exports = (questions, done = f => f) => {
+module.exports = (questions, done = (f) => f) => {
   const answers = [];
   const [firstQuestion] = questions;
 
-  const questionAnswered = answer => {
+  const questionAnswered = (answer) => {
     answers.push(answer);
     if (answers.length < questions.length) {
       rl.question(questions[answers.length], questionAnswered);
