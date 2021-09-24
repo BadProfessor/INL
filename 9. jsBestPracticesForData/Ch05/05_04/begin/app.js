8f0a28a5210204cce1c2dceae377fcd8649fa133
Juraj Jarmek <badjjj@live.com>
2021-09-24 10:44:31 +0100

    test

--- 9. jsBestPracticesForData/Ch05/05_04/begin/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const data = {
-  warning: true,
-  warningText: 'Solar flare may disrupt communications',
-};
-const warning = data.warning ? true : false;
-
-console.log(`Warning: ${(warning) ? data.warningText : 'No warning message at this time'}`);
