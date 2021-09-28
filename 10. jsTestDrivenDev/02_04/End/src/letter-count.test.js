af046e2825bb12cc05e7b628881b02a66a509d30
jjcoke <87483334+jjcoke@users.noreply.github.com>
2021-09-28 17:10:54 +0100

    test

--- 10. jsTestDrivenDev/02_04/End/src/letter-count.test.js
+++ 10. jsTestDrivenDev/02_04/End/src/letter-count.test.js
@@ -2,9 +2,9 @@ import { expect } from 'chai';
 import { getLetterCount } from './letter-count';
 
 describe('getLetterCount - basic functionality', () => {
-    it('returns an empty object when passed an empty string', () => {
-        const expected = {};
-        const actual = getLetterCount('');
-        expect(actual).to.deep.equal(expected);
-    })
-})
+  it('returns an empty object when passed an empty string', () => {
+    const expected = {};
+    const actual = getLetterCount('');
+    expect(actual).to.deep.equal(expected);
+  });
+});
