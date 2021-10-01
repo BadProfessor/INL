ddcce0ac3f63832c47d3a53490ae8dbeec78bd48
jjcoke <87483334+jjcoke@users.noreply.github.com>
2021-10-01 11:04:20 +0100

    test

--- 10. jsTestDrivenDev/05_02/End/src/db.js
+++ 10. jsTestDrivenDev/05_02/End/src/db.js
@@ -1,18 +1,18 @@
 import { MongoClient } from 'mongodb';
 
-export const getUserByUsername = async username => {
-    const client = await MongoClient.connect(
-        `mongodb://localhost:27017/TEST_DB`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        },
-    );
-    const db = client.db('TEST_DB');
+export const getUserByUsername = async (username) => {
+  const client = await MongoClient.connect(
+    `mongodb://localhost:27017/TEST_DB`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  const db = client.db('TEST_DB');
 
-    const result = await db.collection('users').findOne({ username });
+  const result = await db.collection('users').findOne({ username });
 
-    client.close();
+  client.close();
 
-    return result;
-}
+  return result;
+};
--- 10. jsTestDrivenDev/05_02/End/src/db.test.js
+++ 10. jsTestDrivenDev/05_02/End/src/db.test.js
@@ -3,39 +3,42 @@ import { expect } from 'chai';
 import { getUserByUsername } from './db';
 
 describe('getUserByUsername', () => {
-    it('get the correct user from the database given a username', async () => {
-        const client = await MongoClient.connect(
-            `mongodb://localhost:27017/TEST_DB`,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-        );
-        const db = client.db('TEST_DB');
+  it('get the correct user from the database given a username', async () => {
+    const client = await MongoClient.connect(
+      `mongodb://localhost:27017/TEST_DB`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    const db = client.db('TEST_DB');
 
-        const fakeData = [{
-            id: '123',
-            username: 'abc',
-            email: 'abc@gmail.com',
-        }, {
-            id: '124',
-            username: 'wrong',
-            email: 'wrong@wrong.com',
-        }];
+    const fakeData = [
+      {
+        id: '123',
+        username: 'abc',
+        email: 'abc@gmail.com',
+      },
+      {
+        id: '124',
+        username: 'wrong',
+        email: 'wrong@wrong.com',
+      },
+    ];
 
-        await db.collection('users').insertMany(fakeData);
-        const actual = await getUserByUsername('abc');
-        const finalDBState = await db.collection('users').find().toArray();
-        await db.dropDatabase();
-        client.close();
+    await db.collection('users').insertMany(fakeData);
+    const actual = await getUserByUsername('abc');
+    const finalDBState = await db.collection('users').find().toArray();
+    await db.dropDatabase();
+    client.close();
 
-        const expected = {
-            id: '123',
-            username: 'abc',
-            email: 'abc@gmail.com',
-        };
+    const expected = {
+      id: '123',
+      username: 'abc',
+      email: 'abc@gmail.com',
+    };
 
-        expect(actual).to.deep.equal(expected);
-        expect(finalDBState).to.deep.equal(fakeData);
-    });
+    expect(actual).to.deep.equal(expected);
+    expect(finalDBState).to.deep.equal(fakeData);
+  });
 });
