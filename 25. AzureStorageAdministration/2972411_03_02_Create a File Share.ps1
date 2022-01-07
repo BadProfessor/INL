77c83e077cee74338422fae57ff4a56a8d7901f2
BadProfessor <badjjj@live.com>
2022-01-07 15:35:46 +0000

    test

---
+++ 25. AzureStorageAdministration/2972411_03_02_Create a File Share.ps1
@@ -0,0 +1,50 @@
+﻿#Create a file share
+
+#Variables
+$location = "canadacentral"
+$resourceGroup = "AZ104Demo_ps"
+$storageAccountName = "az104fsdemo"
+$FileSharename = "hrfiles"
+$Path = "recruitment"
+$Sharename = "hrfiles"
+
+# Retrieve the context
+$Key = (Get-AzStorageAccountKey -ResourceGroupName $resourcegroup -StorageAccountName $storageAccountName).Value[0]
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $Key
+
+#Create a file share
+New-AzStorageShare -Name $FileSharename -Context $ctx
+
+#View file shares
+Get-AzStorageShare -Context $ctx
+
+#Create a new directory 
+New-AzStorageDirectory -ShareName $FileSharename -Path $Path -Context $ctx 
+
+#Upload a file to the Azure file Share 
+Set-AzStorageFileContent -ShareName $Sharename -Source "C:\Users\Sharon Bennett\Desktop\AZ-104\Scripts\doc2.txt" -Path $path -Context $ctx
+
+#View files
+Get-AzStorageFile -ShareName $Sharename -path $Path -Context $ctx | Get-AzStorageFile
+
+#Download a file to the local system 
+Get-AzStorageFileContent -ShareName $Sharename -Path "recruitment\doc2.txt" -Destination "C:\Users\Sharon Bennett\Desktop\AZ-104\Scripts\Doc3.txt" -Context $ctx
+
+#Delete the file share
+Remove-AzStorageShare -Name $FileSharename -Context $ctx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#object containting references to the storage account. 
