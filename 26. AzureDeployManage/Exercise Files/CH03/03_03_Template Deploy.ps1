b96f94e93387215251c5d70a56b65d1a4ee440a5
BadProfessor <badjjj@live.com>
2022-01-07 16:38:13 +0000

    test

---
+++ 26. AzureDeployManage/Exercise Files/CH03/03_03_Template Deploy.ps1
@@ -0,0 +1,7 @@
+﻿#Upload and deploy a template
+
+New-AzResourceGroup -Name NewTemplateDeployment -Location eastus
+
+New-AzResourceGroupDeployment -ResourceGroupName 'NewTemplateDeployment' `
+-TemplateFile 'C:\Users\Sharon Bennett\Desktop\AZ-104\Template\template.json' `
+-TemplateParameterFile 'C:\Users\Sharon Bennett\Desktop\AZ-104\Template\parameters.json'
---
+++ 26. AzureDeployManage/Exercise Files/CH04/04_03_CreateAContainer.ps1
@@ -0,0 +1,7 @@
+﻿  
+#Create a container
+
+New-AzContainerGroup -ResourceGroupName Containers `
+-Name mycontainer `
+-Image mcr.microsoft.com/windows/servercore/iis:nanoserver `
+-OsType Windows -DnsNameLabel az104container-demo-win
