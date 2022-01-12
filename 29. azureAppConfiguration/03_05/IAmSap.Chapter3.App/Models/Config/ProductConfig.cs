e9c63bdaf958a01e389c0378a47f8642f5d10084
BadProfessor <badjjj@live.com>
2022-01-12 11:19:44 +0000

    test

--- 29. azureAppConfiguration/03_05/IAmSap.Chapter3.App/Models/Config/ProductConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IAmSap.Chapter3.App.Models.Config
-{
-    public class ProductConfig
-    {
-        public string ServiceUrl { get; set; }
-        public int RowCount { get; set; }
-    }
-}
