85899722cd66f74ec4b2d5340497afaedc5082bb
Juraj <badjjj@live.com>
2022-03-16 20:08:04 +0000

    test

--- 23. Swift5Essentials/AdventureSwift_Working/Ch2_Variable_Basics.playground/Pages/Working with strings.xcplaygroundpage/Contents.swift
+++ 23. Swift5Essentials/AdventureSwift_Working/Ch2_Variable_Basics.playground/Pages/Working with strings.xcplaygroundpage/Contents.swift
@@ -13,13 +13,21 @@
  
  */
 // Test variable
-
+var dialogue = "the Innkeeper's Haven"
 
 // String data
-
+dialogue.count
+dialogue.isEmpty
+dialogue.contains("s")
 
 // Append and Insert
-
+dialogue.append(contentsOf: ", weary travelers!")
 
 // Remove and Split
+dialogue.removeLast()
+dialogue.removeFirst()
+dialogue.removeAll()
 
+dialogue.split(separator: ",")
+
+print(dialogue)
