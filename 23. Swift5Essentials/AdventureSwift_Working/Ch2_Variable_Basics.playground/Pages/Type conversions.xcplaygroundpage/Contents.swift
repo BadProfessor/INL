43f061d9bf714eb887860d70d3558a20c899b388
Juraj <badjjj@live.com>
2022-03-21 15:47:03 +0000

    test

--- 23. Swift5Essentials/AdventureSwift_Working/Ch2_Variable_Basics.playground/Pages/Type conversions.xcplaygroundpage/Contents.swift
+++ 23. Swift5Essentials/AdventureSwift_Working/Ch2_Variable_Basics.playground/Pages/Type conversions.xcplaygroundpage/Contents.swift
@@ -14,10 +14,13 @@
  
 */
 // Test variables
-
+var currentGold_Double = 5.832
 
 // Explicit conversions
-
+var currentGold_Integer: Int = Int(currentGold_Double)
+var currentGold_String = String(currentGold_Double)
 
 // Inferred conversion with operators
+var bankDeposit = 37 + 5.892
+var bankDeposit_Explicit = currentGold_Double + Double(currentGold_Integer)
 
