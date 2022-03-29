4e4f1d134fd2043a0b44efdb1ce5f0159b0d9d33
Juraj <badjjj@live.com>
2022-03-29 21:17:08 +0100

    test

--- 23. Swift5Essentials/AdventureSwift_Working/Ch3_Working_with_Collections.playground/Pages/Swift Arrays.xcplaygroundpage/Contents.swift
+++ 23. Swift5Essentials/AdventureSwift_Working/Ch3_Working_with_Collections.playground/Pages/Swift Arrays.xcplaygroundpage/Contents.swift
@@ -15,10 +15,15 @@
  
  */
 // Creating arrays
-
+var levelDifficulty: [String] = ["Easy", "Moderate", "Veteran", "Nightmare"]
+var extraSyntax1: Array<String> = Array<String>()
 
 // Count and isEmpty
-
+levelDifficulty.count
+levelDifficulty.isEmpty
 
 // Accessing array values
+var mostDifficult = levelDifficulty[3]
+levelDifficulty[3] = "Utter Ridiculousness"
 
+//levelDifficulty[4]
