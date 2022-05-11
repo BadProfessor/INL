9aeb318e3d992f9314c8c74e4912762f1866e871
Juraj <badjjj@live.com>
2022-05-11 17:46:18 +0100

    test

--- 25. completeJSCourse/01-Fundamentals-Part-1/starter/scripts.js
+++ 25. completeJSCourse/01-Fundamentals-Part-1/starter/scripts.js
@@ -1,5 +1,5 @@
 /*
-Linking a JS file
+// Linking a JS file
 alert('Hello World!');
 
 let js = 'amazing';
@@ -11,11 +11,25 @@ if (js === 'amazing') alert('JavaScr ipt is fun!');
 let math = 40 + 8 + 23 - 10;
  */
 // Values and variables
-console.log('Jonas');
+/* console.log('Jonas');
 console.log(23);
 
 let firstName = 'Matilda';
 
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+console.log(firstName); */
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+const PI = 3.1415;
+
+let myFirstJob = 'Code';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
