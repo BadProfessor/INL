45d1114ee2502647b32fcad2cd007cbb7853192c
Juraj <badjjj@live.com>
2022-05-11 18:44:47 +0100

    test

--- 25. completeJSCourse/01-Fundamentals-Part-1/final/script.js
+++ 25. completeJSCourse/01-Fundamentals-Part-1/final/script.js
@@ -1,20 +1,4 @@
 /*
-// Variable name conventions
-let jonas_matilda = "JM";
-let $function = 27;
-
-let person = "jonas";
-let PI = 3.1415;
-
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
-
-let job1 = "programmer";
-let job2 = "teacher";
-
-console.log(myFirstJob);
-
-////////////////////////////////////
 // Data Types
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
--- 25. completeJSCourse/01-Fundamentals-Part-1/starter/scripts.js
+++ 25. completeJSCourse/01-Fundamentals-Part-1/starter/scripts.js
@@ -1,35 +1,55 @@
 /*
-// Linking a JS file
-alert('Hello World!');
+Block comment */
+// // Linking a JS file
+// alert('Hello World!');
 
-let js = 'amazing';
+// let js = 'amazing';
 
-if (js === 'amazing') alert('JavaScript is fun!');
+// if (js === 'amazing') alert('JavaScript is fun!');
 
-js = 'boring';
-if (js === 'amazing') alert('JavaScr ipt is fun!');
-let math = 40 + 8 + 23 - 10;
- */
-// Values and variables
-/* console.log('Jonas');
-console.log(23);
+// js = 'boring';
+// if (js === 'amazing') alert('JavaScr ipt is fun!');
+// let math = 40 + 8 + 23 - 10;
+// // Values and variables
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'Matilda';
+// let firstName = 'Matilda';
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName); */
-// Variable name conventions
-let jonas_matilda = 'JM';
-let $function = 27;
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// // Variable name conventions
+// let jonas_matilda = 'JM';
+// let $function = 27;
 
-let person = 'jonas';
-const PI = 3.1415;
+// let person = 'jonas';
+// const PI = 3.1415;
 
-let myFirstJob = 'Code';
-let myCurrentJob = 'Teacher';
+// let myFirstJob = 'Code';
+// let myCurrentJob = 'Teacher';
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+// let job1 = 'programmer';
+// let job2 = 'teacher';
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+// // Data Types
+// let javaScriptisFun = true;
+// console.log(javaScriptisFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javaScriptisFun = 'YES!';
+// console.log(typeof javaScriptisFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
