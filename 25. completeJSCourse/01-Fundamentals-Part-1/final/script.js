32db3e03646e62b7fc2edfcbd4184ea8a679264d
Juraj <badjjj@live.com>
2022-05-11 14:57:55 +0100

    test

--- 25. completeJSCourse/01-Fundamentals-Part-1/final/script.js
+++ 25. completeJSCourse/01-Fundamentals-Part-1/final/script.js
@@ -1,20 +1,4 @@
 /*
-////////////////////////////////////
-// Linking a JavaScript File
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-
-////////////////////////////////////
-// Values and Variables
-console.log("Jonas");
-console.log(23);
-
-let firstName = "Matilda";
-
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-
 // Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
--- 25. completeJSCourse/01-Fundamentals-Part-1/starter/scripts.js
+++ 25. completeJSCourse/01-Fundamentals-Part-1/starter/scripts.js
@@ -1,10 +1,21 @@
-/* alert('Hello World!');
+/*
+Linking a JS file
+alert('Hello World!');
 
 let js = 'amazing';
 
 if (js === 'amazing') alert('JavaScript is fun!');
 
 js = 'boring';
-if (js === 'amazing') alert('JavaScript is fun!');
+if (js === 'amazing') alert('JavaScr ipt is fun!');
 let math = 40 + 8 + 23 - 10;
  */
+// Values and variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
