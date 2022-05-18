31f25670287062c0242687f1438d81fca8b67203
Juraj <badjjj@live.com>
2022-05-18 14:46:15 +0100

    test

--- 25. completeJSCourse/01-Fundamentals-Part-1/starter/script.js
+++ 25. completeJSCourse/01-Fundamentals-Part-1/starter/script.js
@@ -238,3 +238,49 @@ Block comment
 // }
 
 // Equality operators: == vs. ===
+// const age = '18';
+// if (age === 18) console.log(`You just became an adult :D (strict)`);
+
+// if (age == 18) console.log(`You just became an adult :D (loose)`);
+
+// const favourite = Number(prompt(`What's your favorite number?`));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite === 23) {
+//   // 22 === 23 -> FALSE
+//   console.log(`Cool! 23 is an amazing number!`);
+// } else if (favourite === 7) {
+//   console.log(`7 is also a cool number!`);
+// } else if (favourite === 9) {
+//   console.log(`9 is also a cool number!`);
+// } else {
+//   console.log(`Number is not 23 or 7 or 9`);
+// }
+
+// if (favourite !== 23) console.log(`Why not 23?`);
+
+// Logical operators
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// // if (hasDriversLicense && hasGoodVision) {
+// //     console.log(`Sarah is able to drive!`);
+// // } else {
+// //     console.log(`Someone else should drive...`);
+// // }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log(`Sarah is able to drive`);
+// } else {
+//   console.log(`Some else should drive...`);
+// }
+
+// Coding Challenge #3
