66527ac6c6eef23ff00cf053b16c130fe3d2e061
Juraj <badjjj@live.com>
2022-05-23 09:06:35 +0100

    test

--- 25. completeJSCourse/02-Fundamentals-Part-2/starter/script.js
+++ 25. completeJSCourse/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,7 @@
 'use strict';
+/* 
+
+*/
 // // Activating Strict Mode
 // let hasDriversLicense = false;
 // const passTest = true;
@@ -75,3 +78,52 @@
 
 // console.log(fruitProcessor(2, 3));
 // Reviewing Functions
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired`);
+//     return -1;
+//   }
+// };
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+// Coding Coding Challenge #1
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+// // Test 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log(`No team wins...`);
+//   }
+// };
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// checkWinner(576, 111);
+
+// // Test 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// Introduction to Arrays
