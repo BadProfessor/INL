62cceeb3388c39b0a5c61c5672ff0a1e6b508365
Juraj <badjjj@live.com>
2022-06-20 11:24:20 +0100

    tes

--- 26. jsDebugging/Exercise Files/Ch02/02_challenge_2/challenge.js
+++ 26. jsDebugging/Exercise Files/Ch02/02_challenge_2/challenge.js
@@ -1,44 +1,46 @@
 jQuery(function () {
-  "use strict";
+  'use strict';
   // Don't do anything here if cookie is set
-  if (window.sessionStorage.getItem("modalViewed") === true) return;
+  if (window.sessionStorage.getItem('modalViewed') === 'true') return;
 
-  var modalContainer = document.querySelector(".modal--container");
+  var modalContainer = document.querySelector('.modal--container');
 
   // Display the timeout after a couple seconds
   setTimeout(function () {
-    modalContainer.classList.add("open");
+    modalContainer.classList.add('open');
 
     // note that the user has seen this modal before
-    window.sessionStorage.setItem("modalViewed", true);
+    window.sessionStorage.setItem('modalViewed', 'true');
 
     setTimeout(function () {
-      modalContainer.classList.add("visible");
+      modalContainer.classList.add('visible');
     }, 100);
   }, 2000);
 
   // Close the modal via the Escape key
-  document.addEventListener("keyup", function (evt) {
-    if (evt.which !== 27 || !modalContainer.classList.contains("visible"))
+  document.addEventListener('keyup', function (evt) {
+    if (evt.which !== 27 || !modalContainer.classList.contains('visible'))
       return;
 
     hideModal();
   });
 
   // Close the modal when clicking Close
-  document.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  modalContainer
+    .querySelector('.modal--close')
+    .addEventListener('click', function (evt) {
+      evt.preventDefault();
 
-    hideModal();
-  });
+      hideModal();
+    });
 
   function hideModal() {
-    modalContainer.classList.remove("visible");
+    modalContainer.classList.remove('visible');
 
     // when browser compatibility is better, could use `transitionend` event here
     // for now, fire this with a timeout
     setTimeout(function () {
-      modalContainer.classList.remove("open");
-    }, 100);
+      modalContainer.classList.remove('open');
+    }, 2100);
   }
 });
