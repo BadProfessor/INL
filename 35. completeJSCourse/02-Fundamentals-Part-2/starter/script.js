fbde1c2a3d74a2f65a4c46a5cd149e1f2cff2dce
Juraj <badjjj@live.com>
2022-07-16 17:34:04 +0100

    test

--- 35. completeJSCourse/02-Fundamentals-Part-2/starter/script.js
+++ 35. completeJSCourse/02-Fundamentals-Part-2/starter/script.js
@@ -169,4 +169,28 @@
 // ];
 // console.log(ages);
 
-// Basic Array Operations
+// Basic Array Operations (Methods)
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// // Add Elements
+// const newLength = friends.push('Jay');
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift('John');
+// console.log(friends);
+
+// friends.shift(); // First
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
+
+// friends.push(23);
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+// console.log(friends.includes(23));
+
+// if (friends.includes('Steven')) {
+//   console.log(`You have a friend called Steven`);
+// }
