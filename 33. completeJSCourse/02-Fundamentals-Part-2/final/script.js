76fa4cf895c1cad0747de3af0a5d639cc085cf73
Juraj <badjjj@live.com>
2022-07-30 09:18:53 +0100

    test

--- 33. completeJSCourse/02-Fundamentals-Part-2/final/script.js
+++ 33. completeJSCourse/02-Fundamentals-Part-2/final/script.js
@@ -1,26 +1,5 @@
 'use strict';
 /*
-// The while Loop
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
-}
-
-let rep = 1;
-while (rep <= 10) {
-  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
-  rep++;
-}
-
-let dice = Math.trunc(Math.random() * 6) + 1;
-
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log('Loop is about to end...');
-}
-*/
-
-///////////////////////////////////////
 // Coding Challenge #4
 
 /*
--- 33. completeJSCourse/02-Fundamentals-Part-2/starter/script.js
+++ 33. completeJSCourse/02-Fundamentals-Part-2/starter/script.js
@@ -385,3 +385,20 @@
 //     console.log(`Exercise ${exercise}: Lifting weight repetition: ${rep}`);
 //   }
 // }
+// The while Loop
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//   rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log('Loop is about to end...');
+// }
